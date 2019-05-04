60e783647fc11e089d296f4140a2b5fbe31f728b
Leandro Moro <33632804+moroleandro@users.noreply.github.com>
2019-05-04 19:31:57 -0300

    Estrutura API

---
+++ src/models/Box.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+
+const Box = new mongoose.Schema({
+    title:{
+        type: String,
+        required: true,
+    },
+    files:[]
+},
+{
+    timestamps: true
+});
+
+module.exports = mongoose.model("Box", Box);
